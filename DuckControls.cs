30b6947d541f1e9f7e7c7a2212631bc0b85ceb92
James Conlin <jacon.dev@outlook.com>
2022-01-24 21:22:52 +0000

    Initial version

---
+++ DuckControls.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework.Constraints;
+
+namespace LiskovSubstitutionPrinciple
+{
+    public class DuckControls
+    {
+        public bool MathTheDuckSwim(Duck duck)
+        {
+            var isSwimming = duck.Swim();
+            return isSwimming;
+        }
+
+        public string MakeTheDuckQuack(Duck duck)
+        {
+            var duckQuack = duck.MakeNoise();
+            return duckQuack;
+        }
+    }
+
+    public abstract class Duck
+    {
+        public abstract bool Swim();
+        public abstract string MakeNoise();
+    }
+
+    public class RealDuck : Duck
+    {
+        public override bool Swim()
+        {
+            return true;
+        }
+
+        public override string MakeNoise()
+        {
+            return "Quack!";
+        }
+    }
+
+    public class ToyDuck : Duck
+    {
+        private bool _isTurnedOn;
+        
+        public override bool Swim()
+        {
+            if (_isTurnedOn) return true;
+            return false;
+        }
+
+        public override string MakeNoise()
+        {
+            if (_isTurnedOn) return "Techno Quack!";
+            return null;
+        }
+    }
+}
---
+++ DuckControlsTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace LiskovSubstitutionPrinciple
+{
+    [TestFixture]
+    public class DuckControlsTests
+    {
+        [Test]
+        public void TestThatRealDuckCanSwim()
+        {
+            var sut = new DuckControls();
+            var duck = new RealDuck();
+            
+            Assert.IsTrue(sut.MathTheDuckSwim(duck));
+        }        
+        
+        [Test]
+        public void TestThatRealDuckQuacks()
+        {
+            var sut = new DuckControls();
+            var duck = new RealDuck();
+            
+            Assert.AreEqual("Quack!", sut.MakeTheDuckQuack(duck));
+        }
+
+        [Test]
+        public void TestThatToyDuckCanSwim()
+        {
+            var sut = new DuckControls();
+            var duck = new ToyDuck();
+            
+            Assert.IsTrue(sut.MathTheDuckSwim(duck));
+        }
+
+        [Test]
+        public void TestThatToyDuckQuacks()
+        {
+            var sut = new DuckControls();
+            var duck = new ToyDuck();
+            
+            Assert.AreEqual("Techno Quack!", sut.MakeTheDuckQuack(duck));
+        }
+    }
+}
